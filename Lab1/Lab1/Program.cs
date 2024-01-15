d9d82de368b8cdb046e95a78875c540c76e7c745
W0468743 <runasjosh12@yahoo.com>
2024-01-15 11:33:45 -0400

    My new Project

---
+++ Lab1/Lab1/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Lab1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hi I hope I pass");
+        }
+    }
+}
